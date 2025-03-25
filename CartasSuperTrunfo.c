fb39e1bf776e7b8e32bc8b420676a091a99020d2
LuizPedro89 <Pedrorocha7062@outlook.com>
2025-03-25 20:57:18 +0000

    comit novo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,22 @@ int main() {
     pintf("Desafio Xadrez!/n");
 }
 Estado (char): Rio de Janeiro
-Codigo da carta(char[]):R01 R02 R03
-Nome da cidade(char[]):Rio de Janeiro
+Codigo da Carta(char[]):R01 R02 R03
+Nome da Cidade(char[]):Rio de Janeiro
 Populaçao (int):6000000
 Área em km (float):43000
 Pib (float):1000000000000
 Número de Pontos TurÍsticos:58
+
+     printf("Novo Comit!/n")
+
+     Estado(Char[]): Pernambuco
+     Código da Carta(Char[]): P1 P2 P3
+     Nome da cidade(cahr[]):Recife
+     Populaçao(int):9000000
+     Área em km(float): 98000
+     Pib (float):250000000000
+     Número de Pontos Turísticos(int):40
 
 
 
